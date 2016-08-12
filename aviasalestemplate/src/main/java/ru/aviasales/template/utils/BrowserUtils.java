f70404a14cb26c62756b55f93b6c72ebd61b4233
KP <igor.yurchenko@aviasales.ru>
2016-08-12 19:17:58 +0300

    fix ads

--- aviasalestemplate/src/main/java/ru/aviasales/template/utils/BrowserUtils.java
+++ aviasalestemplate/src/main/java/ru/aviasales/template/utils/BrowserUtils.java
@@ -18,25 +18,35 @@ public class BrowserUtils {
 	                               @Nullable String title,
 	                               @Nullable String host,
 	                               boolean showLoadingDialog) {
-		if (activity == null || url == null || title == null) return;
+		if (url == null || title == null) return;
 
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-			Utils.getPreferences(activity)
-					.edit()
-					.putString(BrowserFragment.URL, url)
-					.putString(BrowserFragment.TITLE, title)
-					.commit();
-			launchInternalBrowser(activity, showLoadingDialog, host);
+			openInternalBrowser(activity, url, title, host, showLoadingDialog);
 		} else {
-			Intent intent = new Intent(Intent.ACTION_VIEW);
-			intent.setData(Uri.parse(url));
-			if (host != null) {
-				Bundle bundle = new Bundle();
-				bundle.putString(BrowserFragment.REFERER_HEADER, BrowserFragment.HTTP + host);
-				intent.putExtra(Browser.EXTRA_HEADERS, bundle);
-			}
-			activity.startActivity(intent);
+			openExternalBrowser(activity, url, host);
 		}
+	}
+
+	public static void openExternalBrowser(@Nullable Activity activity, @Nullable String url, @Nullable String host) {
+		if (activity == null) return;
+		Intent intent = new Intent(Intent.ACTION_VIEW);
+		intent.setData(Uri.parse(url));
+		if (host != null) {
+			Bundle bundle = new Bundle();
+			bundle.putString(BrowserFragment.REFERER_HEADER, BrowserFragment.HTTP + host);
+			intent.putExtra(Browser.EXTRA_HEADERS, bundle);
+		}
+		activity.startActivity(intent);
+	}
+
+	public static void openInternalBrowser(@Nullable Activity activity, @Nullable String url, @Nullable String title, @Nullable String host, boolean showLoadingDialog) {
+		if (activity == null) return;
+		Utils.getPreferences(activity)
+				.edit()
+				.putString(BrowserFragment.URL, url)
+				.putString(BrowserFragment.TITLE, title)
+				.commit();
+		launchInternalBrowser(activity, showLoadingDialog, host);
 	}
 
 	private static void launchInternalBrowser(Activity activity, boolean showLoadingDialog, String host) {
