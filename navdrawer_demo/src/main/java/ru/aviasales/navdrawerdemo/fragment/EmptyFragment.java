310d23069aba741848851f4c85fa5a74cb97d36a
kpeved <uakpevedko@gmail.com>
2016-12-08 18:25:04 +0300

    fixed bug with actionBar in navDrawerTemplate

--- navdrawer_demo/src/main/java/ru/aviasales/navdrawerdemo/fragment/EmptyFragment.java
+++ navdrawer_demo/src/main/java/ru/aviasales/navdrawerdemo/fragment/EmptyFragment.java
@@ -2,6 +2,8 @@ package ru.aviasales.navdrawerdemo.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,8 +16,22 @@ public class EmptyFragment extends Fragment {
 	public static final String TAG = "empty_fragment";
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
+	}
 
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		setActionBarTitle();
 		return inflater.inflate(R.layout.content_main, null);
 	}
+
+	private void setActionBarTitle() {
+		ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+		if (actionBar != null) {
+			actionBar.setTitle("Empty fragment");
+		}
+	}
+
 }
